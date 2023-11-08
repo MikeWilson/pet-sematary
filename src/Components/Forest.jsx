f47ebcf8bb72a795a53cd16365d105946abe99c8
Mike Wilson <mike.wilson983@gmail.com>
2023-11-08 14:16:56 -0700

    ugh

--- src/Components/Forest.jsx
+++ src/Components/Forest.jsx
@@ -1,73 +1,89 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Canvas, useFrame, useThree, useLoader} from '@react-three/fiber';
-import { Vector3 } from 'three';
-import { useGLTF } from '@react-three/drei';
-import { MeshBasicMaterial } from 'three';
+import React, { useMemo, useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import { MeshBasicMaterial, Vector3 } from "three";
 
+const Tree = React.memo(({ position, scale, gltf }) => {
+  const blackMaterial = useMemo(() => new MeshBasicMaterial({ color: "black" }), []);
+  const model = useRef(gltf.scene.clone()).current;
 
-// Tree component with simple box geometry
-// const Tree = ({ position }) => (
-//   <>
-//   <mesh position={position}>
-//     <boxGeometry args={[1, 15, 1]} />
-//     <meshStandardMaterial color="brown" />
-//   </mesh>
-//   <mesh position={[position.x, position.y + 5, position.z]}>
-//     <sphereGeometry args={[5]} position={[0,20,0]} />
-//     <meshStandardMaterial color="green" />
-//   </mesh>
-//   </>
-// );
-
-const Tree = ({ position, gltf }) => {
-  const blackMaterial = new MeshBasicMaterial({ color: 'black' });
-  const model = gltf.scene.clone();
-  
   return (
-    <group position={position} dispose={null} scale={[5, 7, 5]}>
-      <primitive object={model} material={blackMaterial}/>
+    <group position={position} dispose={null} scale={scale}>
+      <primitive object={model} material={blackMaterial} />
     </group>
   );
+});
+
+const calculateHillHeight = (x, z) => {
+  const frequency = 3; // The frequency of the hills
+  const amplitude = 1; // The amplitude (height) of the hills
+  return Math.sin(frequency * (x + z)) * amplitude;
 };
 
-// Forest component that manages the pool of trees
 const Forest = ({ treeCount, spread }) => {
-  const gltf = useGLTF('./redwood.glb', true);
+  const gltf = useGLTF("./redwood.glb", true);
+  const modelOriginalHeight = 0.5935667157173157; // Actual model height
 
-  // Generate initial positions for the trees
-  const initialPositions = new Array(treeCount).fill().map(() => ({
-    x: (Math.random() - 0.5) * spread  * 3,
-    y: 0,
-    z: (Math.random() - 0.5) * spread * 10,
-  }));
+  // Generate initial positions and scales for the trees
+  const initialTrees = useMemo(() => {
+    return new Array(treeCount).fill().map(() => {
+      const x = (Math.random() - 0.5) * spread * 3;
+      const z = (Math.random() - 0.5) * spread * 10;
+      const desiredHeight = Math.random() * 2 + 0.5; // Random height between 0.5 and 2.5
+      const scaleY = desiredHeight / modelOriginalHeight; // Calculate the scale factor for y-axis
+      const hillHeight = calculateHillHeight(x, z); // Get the hill height for this tree's position
+  
+      return {
+        position: new Vector3(
+          x,
+          (desiredHeight / 2) + hillHeight, // Position the base at the hill height
+          z
+        ),
+        scale: [1, scaleY, 1], // Apply the scale factor only to the y-axis
+      };
+    });
+  }, [treeCount, spread]);
 
-  const [trees, setTrees] = useState(initialPositions);
+  const [trees, setTrees] = useState(initialTrees);
 
-  const { camera, mouse } = useThree()
-  // Update tree positions based on the camera's z position
+  const { camera } = useThree();
+
   useFrame(() => {
-    const cameraZ = camera.position.z;
     setTrees((currentTrees) =>
       currentTrees.map((tree) => {
-        if (tree.z > cameraZ) {
-          // Tree is behind the camera, respawn ahead
+        if (tree.position.z > camera.position.z) {
+          const x = (Math.random() - 0.5) * spread * 3;
+          const z = camera.position.z - (Math.random() * 30 + 30);
+          const desiredHeight = Math.random() * 2 + 0.5;
+          const scaleY = desiredHeight / modelOriginalHeight;
+          const hillHeight = calculateHillHeight(x, z); // Get the new hill height for the respawned tree
+  
           return {
             ...tree,
-            x: camera.position.x + (Math.random() - 0.5) * spread * 3, // Adjust x spawn limits based on camera's x position
-            z: cameraZ - (Math.random() * 30 + 30), // Random number between 30 and 60 away from the camera's z position
+            position: new Vector3(
+              camera.position.x + x,
+              (desiredHeight / 2) + hillHeight, // Adjust the y position based on the hill height
+              z
+            ),
+            scale: [1, scaleY, 1],
           };
         }
         return tree; // Tree is still ahead of the camera
-      }),
+      })
     );
   });
 
   return (
     <>
-    {trees.map((tree, index) => (
-      <Tree key={index} position={new Vector3(tree.x, tree.y, tree.z)}  gltf={gltf} />
-    ))}
-  </>
+      {trees.map((tree, index) => (
+        <Tree
+          key={index}
+          position={tree.position}
+          scale={tree.scale}
+          gltf={gltf}
+        />
+      ))}
+    </>
   );
 };
 
